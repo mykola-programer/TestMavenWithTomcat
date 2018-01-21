da241b94d81584e4dee0df02e02dfaa928cd8cbe
mykola-programer <31999999+mykola-programer@users.noreply.github.com>
2018-01-21 21:50:27 +0200

    Nothing.

--- src/main/java/ua/nike/learn/AOP_Principles.java
+++ src/main/java/ua/nike/learn/AOP_Principles.java
@@ -10,25 +10,27 @@ public class AOP_Principles extends HttpServlet {
 
     private String http_out_inf =
             "<!DOCTYPE HTML>\n" +
-            "<html>\n" +
-                "<head>\n" +
+                    "<html>\n" +
+                    "<head>\n" +
                     "<meta charset=\"UTF-8\">\n" +
                     "<title>AOP Principles</title>\n" +
-                "</head>\n" +
-                "<body>\n" +
+                    "</head>\n" +
+                    "<body>\n" +
                     "<h3><a href=\"https://docs.oracle.com/javase/tutorial/java/concepts/index.html\" target=\"_blank\">Aspect Oriented Programming Principles :</a></h3>\n" +
-                        "<ul>\n" +
-                            "<li><a href=\"https://docs.oracle.com/javase/tutorial/java/concepts/object.html\" target=\"_blank\">Object (Об'єкт)</a></li>\n" +
-                            "<li>Class (Клас)</li>\n" +
-                            "<li>Inheritance (Наслідування)</li>\n" +
-                            "<li>Interface (Інтерфейс)</li>\n" +
-                            "<li>Package (Пакет)</li>\n" +
-                        "</ul>\n" +
-                "</body>\n" +
-            "</html>\n";
+                    "<ul>\n" +
+                    "<li><a href=\"https://docs.oracle.com/javase/tutorial/java/concepts/object.html\" target=\"_blank\">Object (Об'єкт)</a></li>\n" +
+                    "<li>Class (Клас)</li>\n" +
+                    "<li>Inheritance (Наслідування)</li>\n" +
+                    "<li>Interface (Інтерфейс)</li>\n" +
+                    "<li>Package (Пакет)</li>\n" +
+                    "</ul>\n" +
+                    "</body>\n" +
+                    "</html>\n";
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
+        String responseContentType = "text/html;charset=UTF-8";
+        response.setContentType(responseContentType);
         response.getWriter().write(http_out_inf);
     }
 }
--- src/main/java/ua/nike/learn/Principles_OOP.java
+++ src/main/java/ua/nike/learn/Principles_OOP.java
@@ -10,22 +10,23 @@ public class Principles_OOP extends HttpServlet {
 
     private String http_out_inf =
             "<!DOCTYPE HTML>\n" +
-            "<html>\n" +
-                "<head>\n" +
-                    "<meta charset=\"UTF-8\">\n" +
-                    "<title>Principles_OOP</title>\n" +
-                "</head>\n" +
-                "<body>\n" +
-                    "<h2>OOP Principles</h2>\n" +
-                    "<h3>It is four principles of OOP :</h3>\n" +
-                        "<ul>\n" +
-                            "<li>Encapsulation (Інкапсуляція)</li>\n" +
-                            "<li>Inheritance (Наслідування)</li>\n" +
-                            "<li>Polymorphism (Поліморфізм)</li>\n" +
-                            "<li>Abstraction (Абстракція)</li>\n" +
-                        "</ul>\n" +
-                "</body>\n" +
-            "</html>\n";
+                    "<html>\n" +
+                    "   <head>\n" +
+                    "       <meta charset=\"UTF-8\">\n" +
+                    "       <title>Principles_OOP</title>\n" +
+                    "   </head>\n" +
+                    "   <body>\n" +
+                    "           <h2>OOP Principles</h2>\n" +
+                    "           <h3>It is four principles of OOP :</h3>\n" +
+                    "       <ul>\n" +
+                    "           <li>Encapsulation (Інкапсуляція)</li>\n" +
+                    "           <li>Inheritance (Наслідування)</li>\n" +
+                    "           <li>Polymorphism (Поліморфізм)</li>\n" +
+                    "           <li>Abstraction (Абстракція)</li>\n" +
+                    "       </ul>\n" +
+                    "   </body>\n" +
+                    "</html>\n";
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
